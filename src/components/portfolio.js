2d409e8058ae43a2dc477f23caa47f60da3b3896
Collin Prusse <collinprusse@mac.com>
2018-05-29 15:37:31 -0600

    heroku deploy

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -11,15 +11,21 @@ export default () => (
             <p>Responsive expenses tracking application using React and Redux for the front-end, and Firebase for back-end storage and login authentication.</p>
             </div>
             <div className="port-item">
+            <a href="https://weather-app-collin-portfolio.herokuapp.com/" target="_blank">
             <img src="../../images/weather.jpg" alt = "Weather App"/>
+            </a>
             <p>A simple weather search app using a weather API to view trends in the weather. Information is stored in Redux and a third-party library is used to graph out the data.</p>
             </div>
             <div className="port-item">
+            <a href="https://redux-blog-collin-portfolio.herokuapp.com/" target="_blank">
             <img src="../../images/reduxblog.jpg" alt = "Redux Blog"/>
+            </a>
             <p>Simple blog post application created in React that stores posts within the Redux store.</p>
             </div>
             <div className="port-item">
+            <a href="https://indecisionapp-collin-portfolio.herokuapp.com/" target="_blank">
             <img src="../../images/indecision.jpg" alt = "Indecision App"/>
+            </a>
             <p>A to-do type application that stores user input data in the React front-end store, and randomly selects a task to be completed.</p>
             </div>
             </div>
